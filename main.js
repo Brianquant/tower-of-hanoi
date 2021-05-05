337db58a2698f820258e07cd4e058baa11c73017
Brianquant <reifferscheid.brianquant@gmail.com>
2021-05-05 22:38:35 +0200

    rm default functions

--- main.js
+++ main.js
@@ -1,44 +1,16 @@
-// Set Peg to default position
-function defaultPosition_1(xAxis, yAxis) {
-    const peg = document.getElementById("peg-1");
-    peg.style.position = "absolute";
-    peg.style.left = xAxis + "px";
-    peg.style.bottom = yAxis + "px";
-}
-function defaultPosition_2(xAxis, yAxis) {
-    const peg = document.getElementById("peg-2");
-    peg.style.position = "absolute";
-    peg.style.left = xAxis + "px";
-    peg.style.bottom = yAxis + "px";
-}
-function defaultPosition_3(xAxis, yAxis) {
-    const peg = document.getElementById("peg-3");
-    peg.style.position = "absolute";
-    peg.style.left = xAxis + "px";
-    peg.style.bottom = yAxis + "px";
-}
-
-defaultPosition_1(50, 240);
-defaultPosition_2(85, 285);
-defaultPosition_3(125, 327);
-
 // Move possibilities
 
-function movePeg_3(xAxis, yAxis) {
-    const peg_3 = document.getElementById("peg-3");
-    let pegIsClicked = false;
+function moveDisc_3(xAxis, yAxis) {
+    const disc_3 = document.getElementById("disc-3");
+    let discIsClicked = false;
 
-    peg_3.addEventListener("click", function () {
-        pegIsClicked = true;
-        if (pegIsClicked) {
-            peg_3.style.position = "absolute";
-            peg_3.style.left = xAxis + "px";
-            peg_3.style.top = yAxis + "px";
-            console.log(pegIsClicked);
-        } else {
-            return;
-        }
+    disc_3.addEventListener("click", function () {
+        discIsClicked = true;
+        disc_3.style.position = "absolute";
+        disc_3.style.left = xAxis + "px";
+        disc_3.style.top = yAxis + "px";
+        console.log(discIsClicked);
     });
 }
 
-movePeg_3(125, 400);
+moveDisc_3(224, 400);
