ea8bc7d1916e3bdc5d10e1254158177b1bb518bc
Brianquant <reifferscheid.brianquant@gmail.com>
2021-05-07 19:49:57 +0200

    added handleClick function

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
-// Define global vars
-const disc_1 = document.getElementById("disc-1");
-const disc_2 = document.getElementById("disc-2");
-const disc_3 = document.getElementById("disc-3");
+const handleClick = (event, tower) => {
+    // const tower = event.target;
 
-const tower_A = document.getElementById("tower-A");
-const tower_B = document.getElementById("tower-B");
-const tower_C = document.getElementById("tower-C");
+    // Scheiben verschieben
 
-// Select disc
-function selectDisc(disc) {
-    disc.addEventListener("click", function () {
-        w3.addStyle(disc, "background-color", "red");
-    });
-}
-selectDisc(disc_3);
+    // Selektieren #1wie #2Wo #3Was
 
-// move selected disc from A to B
-function move_A_to_B(disc, tower) {
-    tower.addEventListener("click", function () {
-        w3.addStyle(disc, "background-color", "gold");
-        const cloneDisc = disc.cloneNode(true);
-        tower.appendChild(cloneDisc);
-        disc.remove();
-        console.log(tower);
-    });
-}
+    //
+    const discs = tower.querySelectorAll(".disc");
+    console.log("discs", discs);
+    const idx = discs.length - 1;
+    if (discs.length === 0) {
+        return;
+    }
+    const lastDisc = discs[idx];
 
-move_A_to_B(disc_3, tower_B);
+    console.log("lastDisc", lastDisc);
+    console.log("event", event.target);
+
+    if (lastDisc.classList.contains("selected")) {
+        lastDisc.classList.remove("selected");
+    } else {
+        lastDisc.classList.add("selected");
+    }
+};
+const towers = document.querySelectorAll("#container-tower > .tower");
+// Scheiben dynamisch anlegen
+
+// Erkläre Bug: lastDisc = undefinded
+console.log("towers", towers);
+towers.forEach((tower) => {
+    tower.addEventListener("click", (event) => handleClick(event, tower));
+});
+
+// function moveDisc(tower) {
+//     tower.addEventListener("click", function () {
+//         while (tower_A.childNodes.length > 0) {
+//             tower.appendChild(tower_A.childNodes[0]);
+//             console.log(tower);
+//         }
+//     });
+// }
+
+// moveDisc(tower_B);
+
+// function move_B_to_C(disc, tower) {
+//     tower.addEventListener("click", function () {
+//         w3.addStyle(disc, "background-color", "gold");
+//         const cloneDisc = disc.cloneNode(true);
+//         tower.appendChild(cloneDisc);
+//         disc.remove();
+//         console.log(tower);
+//     });
+// }
+
+// move_A_to_B(disc_3, tower_C);
