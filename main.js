791e8f395ae1123e7ef79888b3c058f5f946cb12
Brianquant <reifferscheid.brianquant@gmail.com>
2021-05-24 15:57:08 +0200

    added funcitons selectDisc & dropDisc

--- main.js
+++ main.js
@@ -1,30 +1,57 @@
+let clipBoard = null;
+function selectDisc(tower) {
+    // Get Disc from tower
+    const discs = tower.querySelectorAll(".disc");
+    const lastDisc = discs[0];
+    // Put Disc in clipboard
+    clipBoard = lastDisc;
+    console.log("clipBoard", clipBoard);
+}
+
+function dropDisc(tower) {
+    // Take disc from clipboard
+
+    // Drop Disc on tower
+    tower.append(clipBoard);
+}
+
 const handleClick = (event, tower) => {
-    // const tower = event.target;
-    // select all discs
+    const select = event.currentTarget;
+    console.log("select", select);
+    const targetTower = select.closest(".tower");
+    console.log("targetTower", targetTower);
+
     const discs = tower.querySelectorAll(".disc");
     console.log("discs", discs);
 
     if (discs.length === 0) {
-        return;
+        return dropDisc(targetTower);
     }
+
     const lastDisc = discs[0];
-    const getDisc = event.target;
-    console.log("getDisc", getDisc);
     console.log("lastDisc", lastDisc);
-
-    // console.log("event", event.target);
-
     if (lastDisc.classList.contains("selected")) {
         lastDisc.classList.remove("selected");
+        clipBoard = null;
     } else {
         lastDisc.classList.add("selected");
+        selectDisc(tower);
     }
+
+    // const targetTower = select.closest(".tower");
+    // console.log("targetTower", targetTower);
+
+    // dropDisc(tower);
 };
 const towers = document.querySelectorAll("#container-tower > .tower");
 // Erkläre Bug: lastDisc = undefinded
 console.log("towers", towers);
 towers.forEach((tower) => {
-    tower.addEventListener("click", (event) => handleClick(event, tower));
+    tower.addEventListener(
+        "click",
+        (event) => handleClick(event, tower),
+        false
+    );
 });
 
 function decrementWidth(width) {
@@ -39,10 +66,8 @@ function createDisc(width) {
     const targetIndex = document.querySelector(".disc");
     const div = document.createElement("div");
     div.className = "disc";
-    const dataIndex = document.createAttribute("data-index-number");
-    dataIndex.value = incrementIndex(targetIndex.dataset.indexNumber);
-    console.log("targetIndex", targetIndex);
-    div.setAttributeNode(dataIndex);
+    const newIndex = incrementIndex(targetIndex.dataset.indexNumber);
+    div.dataset.indexNumber = newIndex;
     div.style.setProperty("width", width);
     return div;
 }
@@ -54,12 +79,12 @@ function addDisc(tower, disc) {
 
 function getTopDisc(tower) {
     const topDisc = tower.querySelector(".disc");
-    console.log("topDisc", topDisc);
+    // console.log("topDisc", topDisc);
     return topDisc;
 }
 
 const addDiscBtn = document.getElementById("addDisc");
-console.log(addDiscBtn);
+// console.log(addDiscBtn);
 addDiscBtn.addEventListener("click", function () {
     const tower = document.querySelector("#tower-A");
     const newDisc = createDisc(decrementWidth(getTopDisc(tower).clientWidth));
@@ -68,37 +93,16 @@ addDiscBtn.addEventListener("click", function () {
 
 function removeDisc(tower) {
     const discs = tower.querySelectorAll(".disc");
-    console.log("discs", discs);
+    // console.log("discs", discs);
     const lastDisc = discs[0];
     lastDisc.parentElement.removeChild(lastDisc);
-    console.log("removeLastDisc", lastDisc);
+    return lastDisc;
 }
 
 const removeDiscBtn = document.getElementById("removeDisc");
-console.log("removeDisc", removeDiscBtn);
+// console.log("removeDisc", removeDiscBtn);
 removeDiscBtn.addEventListener("click", function () {
     const tower = document.querySelector("#tower-A");
     const TopDisc = getTopDisc(tower);
     removeDisc(tower, TopDisc);
-});
-
-const towerOne = document.getElementById("tower-A");
-towerOne.addEventListener("click", function () {
-    console.log("Tower A gets clicked");
-});
-
-function getSelectedDisc(tower) {
-    const discs = document.querySelectorAll(".disc");
-    const lastDisc = discs[0];
-    tower.appendChild(lastDisc);
-}
-
-const towerTwo = document.getElementById("tower-B");
-towerTwo.addEventListener("click", function () {
-    const receiveDisc = getSelectedDisc(towerTwo);
-    console.log("receiveDisc", receiveDisc);
-});
-const towerThree = document.getElementById("tower-C");
-towerThree.addEventListener("click", function () {
-    console.log("Tower C gets clicked");
 });
