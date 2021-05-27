58e0954af4f220623b7686cd0ba94954d3ecd73d
Brianquant <reifferscheid.brianquant@gmail.com>
2021-05-27 14:37:17 +0200

    tried to implementvalidDisc functionality

--- main.js
+++ main.js
@@ -4,32 +4,45 @@ let clipBoard = null;
 const handleClick = (event, tower) => {
     // check for disc
     if (clipBoard !== null) {
-        // Drop disc on tower
         tower.prepend(clipBoard);
     }
+    // const towerWidth = tower.clientWidth;
 
+    // console.log("tower width", towerWidth);
+    // if (clipBoardWidth < towerWidth) {
+    //     // Drop disc on tower
+    //     tower.prepend(clipBoard);
+    //     console.log("clipBoard width", clipBoard.clientWidth);
+    // }
     // tower = event.target; provide all discs
     const discs = tower.querySelectorAll(".disc");
     console.log("discs", discs);
+
     // If tower has no discs, then return an empty array
     if (discs.length === 0) {
         return;
     }
+
     // Get last disc of tower
     const lastDisc = discs[0];
     console.log("lastDisc", lastDisc, lastDisc.dataset.indexNumber);
+
     // If Statement checks for class selected
     if (lastDisc.classList.contains("selected")) {
         // remove class selected from last disc
         lastDisc.classList.remove("selected");
         // Reset clipboard value
         clipBoard = null;
+
         console.log("remove selected", lastDisc, "clipboard", clipBoard);
     } else {
         // add class selected and disc moves to the top
         lastDisc.classList.add("selected");
         // store last disc in clipboard
         clipBoard = lastDisc;
+        // let clipBoardWidth = clipBoard.clientWidth;
+        // console.log("clipBoardIdx", clipBoardIdx);
+
         console.log(
             "added selected",
             lastDisc,
